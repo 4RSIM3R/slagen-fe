b18d35b819205f00f1a78cebc7ab11440532cc6d
ilzam <ilzammulkhaq85@gmail.com>
2024-05-03 19:22:24 +0700

    feat: first blood

--- src/pages/tasks/components/columns.tsx
+++ src/pages/tasks/components/columns.tsx
@@ -8,30 +8,7 @@ import { priorities, statuses } from '../data/data'
 import { Task } from '../data/schema'
 
 export const columns: ColumnDef<Task>[] = [
-  {
-    id: 'select',
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && 'indeterminate')
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label='Select all'
-        className='translate-y-[2px]'
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label='Select row'
-        className='translate-y-[2px]'
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
+
   {
     accessorKey: 'id',
     header: ({ column }) => (
